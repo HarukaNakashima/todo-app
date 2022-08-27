2e98dc1806937ca03f1ebf833ef1b58e800adf38
Haruka Nakashima <haruchanchan11@gmail.com>
2022-08-27 17:43:45 +0900

    リセットボタンの削除

--- front/pages/components/taskList.js
+++ front/pages/components/taskList.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter, Route } from "next/router";
+import { useRouter } from "next/router";
 import React from "react"
 import axios from "axios"
 
@@ -15,13 +15,10 @@ export default function TaskList () {
     //useStateの宣言
     //tasks:状態変数、setTasks:状態変数を健康するための関数、React.useState(状態変数の初期値)
     const [tasks, setTasks] = React.useState([]);
-    console.log("tasks : ", tasks);//[]配列
-    console.log("test1");
     //APIからの情報取得
     React.useEffect(() => {
         axios.get(baseURL).then((response) => {
             setTasks(response.data);
-            console.log("response.data", response.data);//[{...}, {...}]の形でAPI情報が入ってきている
         });
         //第2引数に値の配列を指定 => マウント時と指定された値に変化があった場合のみに第１引数の関数を実行。
         // todoが追加され、tasksのテーブルに変化　=> 最後リクエストして、リスポンスを表示
@@ -31,8 +28,7 @@ export default function TaskList () {
     
     //todoの中身を一つずつ取り出してそれぞれを別々に表示させる。好きな食べ物の表示方法を参考にする。
     const useTasks = tasks.map((task) => {
-        
-        console.log("task.completed", task.completed);
+        // console.log("task.completed", task.completed);
         //データの完了未完了
         // const completed = () => {
         //     if (task.completed === false) {
