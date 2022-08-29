b1cb397877626e1cd3e1dd1338ea9a43405f5753
Haruka Nakashima <haruchanchan11@gmail.com>
2022-08-29 12:48:24 +0900

    DBのcategoryテーブルから情報を取得し、表示させる機能を追加

--- front/pages/components/taskForm.js
+++ front/pages/components/taskForm.js
@@ -1,20 +1,37 @@
 import Link from "next/link";
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
 import axios from "axios"
+import "/styles/Home.module.css"
 
 const baseURL = "http://localhost:4000/tasks";
+const categoryURL = `http://localhost:4000/categories`;
 
 //taskの新規登録をする関数
 // //TODO:postの入力はどのように実装すべきか考えて実装する。
 export default function Taskform() {
   /* ↓state変数を定義 */
   const [categoryId, setCategoryId] = useState("");
+  const [categories,setCategories] = useState([]);
   const [addCategoryId, setAddCategoryId] = useState(""); 
   const [title, setTitle] = useState("");
   const [addTitle, setAddTitle] = useState("");  
   const [detail, setDetail] = useState("");
   const [addDetail, setAddDetaile] = useState("");  
   
+  useEffect(() => {
+      axios.get(categoryURL).then((response) => {
+        console.log(`${response.data}をGETしました！`);
+        setCategories(response.data);
+      });
+  },[]);
+
+  const useCategories = categories.map((category) => {
+    return (
+        <div className="category-list" key={category.id}>
+        <p>{category.id}:{category.name}</p>
+        </div>
+    );
+  });
   /* ↓関数onClickAddTextを定義 */
   const onClickAddTask = () => {
     console.log("onClickAddTask関数の実行");
@@ -29,13 +46,14 @@ export default function Taskform() {
     //POST
     createTask();
   }
+
   const createTask = () => {
     console.log("createTask関数の実行");
     console.log("categoryId", categoryId);
     console.log("title", title);
     console.log("addDetail", detail);
     const data = {
-      categoryId: Number(categoryId),//数値型に変換することでPOST成功！
+      categoryId: Number(categoryId),
       title: title,
       detail: detail
     }
@@ -56,41 +74,39 @@ export default function Taskform() {
   }
   
   return (
-      <div className="Taskform">
-      <div>
-        <h2>Todo 新登録画面</h2>
-        <p> カテゴリー           
-        <select value={categoryId} 
-                onChange={(e) => setCategoryId(e.target.value)}>
-        <option >1</option>                     
-        <option >2</option>
-        <option >3</option>
-        <option >4</option>                     
-        </select>
-        </p>
-        <p>1:work, 2:private, 3:school, 4:nursery</p>
-        {/* ↓value属性にstate変数「text」を指定 */}
-        <input value={title} 
-               placeholder='タイトル'
-               onChange={(e) => setTitle(e.target.value)}
-        /><br/>
-        <textarea value={detail} 
+      <div className="task-form">
+        <h1>Todo 新登録画面</h1>
+        <div className="task-input">
+          <h3 className="h3">入力フォーム</h3>
+          <p> カテゴリー           
+            <select value={categoryId} 
+                    onChange={(e) => setCategoryId(e.target.value)}>
+              <option >1</option>                     
+              <option >2</option>
+              <option >3</option>
+              <option >4</option>                     
+            </select>
+          </p>
+          <div className="category-container">{useCategories}</div>
+          <input value={title} 
+                 placeholder='タイトル'
+                 onChange={(e) => setTitle(e.target.value)}
+          /><br/>
+          <textarea value={detail} 
                   placeholder='詳しい内容を入力しておこう'
                   onChange={(e) => setDetail(e.target.value)}
-        ></textarea>
-        {/* ↓buttonを追加 */}
-        <button onClick={onClickAddTask}>todoリストに追加</button>
+          ></textarea><br/>
+          <button onClick={onClickAddTask}>todoリストに追加</button>
+        </div>
+        <div className="show-new-task">
+          <h3 className="h3">登録されたtodoの確認画面</h3>
+          <p>カテゴリー：{addCategoryId}</p>
+          <p>タイトル：{addTitle}</p>
+          <p>詳細内容：{addDetail}</p>
+          <button onClick={onClickReset}>確認画面のリセット</button>
+        </div>
+        <Link href={"../"}><a className="back-home">Topに戻る</a></Link>
       </div>
-      <div className="showTask">
-        <h3>登録されたtodoの確認画面</h3>
-        {/* ↓pタグを追加 state変数「addText」を表示する */}
-        <p>カテゴリー：{addCategoryId}</p>
-        <p>タイトル：{addTitle}</p>
-        <p>詳細内容：{addDetail}</p>
-        <button onClick={onClickReset}>確認画面のリセット</button>
-      </div>
-      <Link href={"../"}><a>Topに戻る</a></Link>
-    </div>
   
   );
 }
